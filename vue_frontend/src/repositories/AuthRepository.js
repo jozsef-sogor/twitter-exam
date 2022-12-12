d5355e63316de1359b47e44fc0e6de3603cf3a17
michaliwaniec <mich6h94@stud.kea.dk>
2022-12-12 20:36:14 +0100

    hashing for frontend added

--- vue_frontend/src/repositories/AuthRepository.js
+++ vue_frontend/src/repositories/AuthRepository.js
@@ -1,22 +1,23 @@
-import Client from './Clients/AxiosClient';
+import Client from "./Clients/AxiosClient";
 //const resource = '/posts';
 
 export default {
-    login(form) {
-        return Client.post("/login", form);
-    },
-    logout() {
-        return Client.post("/logout");
-    },
-    register(form) {
-        return Client.post("/register", form);
-    },
-    forgotPassword(form) {
-        return Client.post("/forgot-password", form)
-    },
-    resetPassword(form, token) {
-        return Client.post(`/reset-password?token=${token}`, form)
-    }
+  login(form) {
+    console.log(form);
+    return Client.post("/login", form);
+  },
+  logout() {
+    return Client.post("/logout");
+  },
+  register(form) {
+    return Client.post("/register", form);
+  },
+  forgotPassword(form) {
+    return Client.post("/forgot-password", form);
+  },
+  resetPassword(form, token) {
+    return Client.post(`/reset-password?token=${token}`, form);
+  },
 
-    // MANY OTHER ENDPOINT RELATED STUFFS
+  // MANY OTHER ENDPOINT RELATED STUFFS
 };
